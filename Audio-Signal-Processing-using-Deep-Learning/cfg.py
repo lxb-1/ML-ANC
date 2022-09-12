1f38ef09f61b73c165a9c241e4c4ab4d419c8d78
liangxiaobei <liangxiaobei2016@163.com>
2022-09-12 16:43:43 +0800

    添加MetaAF克隆仓库

--- Audio-Signal-Processing-using-Deep-Learning/cfg.py
+++
@@ -1,8 +0,0 @@
-class Config:
-	def __init__(self, mode='conv', nfilt=26, nfeat=13, nfft=512, rate=16000):
-		self.mode = mode
-		self.nfilt = nfilt
-		self.nfeat = nfeat
-		self.nfft = nfft
-		self.rate = rate
-		self.step = int(rate/10)
